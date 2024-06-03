01cc4e290eb119d86e95cc96b8232de2cfd0b694
Jeanne Zucher <jeanne.zucher@bluemind.net>
2024-06-03 09:16:39 +0200

    create : Banner component

---
+++ src/components/Banner.vue
@@ -0,0 +1,73 @@
+<script setup>
+defineProps({
+  home: {
+    type: Boolean,
+    required: true, 
+    default: false
+  }
+})
+</script>
+
+<template>
+    <div  class="banner" :class="{bannerHome: home}">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.banner {
+	height: 223px;
+	width: 90vw;
+	max-width: 1240px;
+	background: #000;
+	margin: 45px auto 30px;
+	border-radius: 25px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+}
+
+.banner__img {
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	opacity: 0.7;
+	border-radius: 25px;
+}
+
+.banner__title {
+	position: absolute;
+	font-size: 2.75em;
+	font-weight: 500;
+	color: #fff;
+	line-height: 68px;
+}
+
+@media screen and (max-width: 768px) {
+	.banner__title {
+		margin-left: 16px;
+	}
+}
+
+@media screen and (max-width: 576px) {
+	.banner {
+		margin: 0 0 20px 0;
+		border-radius: 10px;
+		justify-content: flex-start;
+	}
+
+	.bannerHome {
+		height: 111px;
+	}
+
+	.banner__img {
+		border-radius: 10px;
+	}
+	.banner__title {
+		font-size: 1.4em;
+		line-height: 24px;
+		width: 70%;
+	}
+}
+</style>
