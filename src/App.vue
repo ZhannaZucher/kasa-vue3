88ca3214cdaf2179a4525a41022a0b493fb45c3b
Jeanne Zucher <jeanne.zucher@bluemind.net>
2024-06-03 08:07:44 +0200

    add : footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 import { RouterView } from 'vue-router'
 
 </script>
@@ -7,6 +8,7 @@ import { RouterView } from 'vue-router'
 <template>
   <Header />
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer class="footer">
+      <div class="footer__logo">
+        <img src="@/assets/logo.svg" alt="logo" />
+      </div>
+      <p class="footer__copyright">© 2024 Kasa. All rights reserved</p>
+    </footer>
+</template>
+
+<style scoped>
+.footer {
+	width: 100vw;
+	height: 200px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-color: #000;
+	color: #fff;
+	font-size: 1.4em;
+	padding: 40px 0 30px;
+	position: absolute;
+	bottom: 0;
+}
+
+.footer__logo img {
+	width: 122px;
+	height: 40px;
+}
+
+.footer__copyright {
+	margin: 20px 0 0;
+}
+
+@media screen and (max-width: 768px) {
+	.footer {
+		font-size: 0.7em;
+	}
+}
+</style>
