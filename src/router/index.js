1b6cb48fe61d29cba3238a0eed7b66593e15e762
Jeanne Zucher <jeanne.zucher@bluemind.net>
2024-06-05 08:54:40 +0200

    add path for accommodation

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
+import AccommodationView from "../views/AccommodationView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/accommodations/:id',
+      name: 'accommodation',
+      props: true,
+      component: AccommodationView
     },
     {
       path: '/:catchAll(.*)',
